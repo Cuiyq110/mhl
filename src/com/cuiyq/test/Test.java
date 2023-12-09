75bb04432f2c10a0f6e830e344987b35cf07bdee
cyq11 <Cuiyq107@gmail.com>
2023-12-09 11:40:42 +0800

    初始化，完成工具类，

---
+++ src/com/cuiyq/test/Test.java
@@ -0,0 +1,21 @@
+package com.cuiyq.test;
+
+import com.cuiyq.utils.JDBCUtilsByDruid;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好");
+
+        try {
+            JDBCUtilsByDruid.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+        }
+
+
+    }
+}
